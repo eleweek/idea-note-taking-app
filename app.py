faad76b9b8399405926a37287e9f6e6347fa75ca
Alexander Putilin <avp-13@yandex.ru>
2015-01-11 22:42:30 +0300

    Hello world in bootstrap

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+Bootstrap(app)
+  
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
